619c0e7f1debe642a1029f2b494714054a428648
Hector Castro <hectcastro@gmail.com>
2020-09-30 11:30:41 -0400

    Load GazetteerCache when app starts

--- src/django/api/apps.py
+++ src/django/api/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        from .matching import GazetteerCache
+        GazetteerCache.get_latest()
